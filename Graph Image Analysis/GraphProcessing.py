4219ac4495308bc4753ec4dacf748c22395c8c12
Gilbert-Three-Stars <118088088+Gilbert-Three-Stars@users.noreply.github.com>
2022-12-21 17:00:14 -0500

    Type Hinting + donwScaleImage
    Added type hinting to every function in the GraphProcessing file, and created a new function that downscales the image to a fixed resolution.
    The purpose of the new function is to make the calculations more efficient.

--- Graph Image Analysis/GraphProcessing.py
+++ Graph Image Analysis/GraphProcessing.py
@@ -2,19 +2,26 @@ import sys
 import numpy as np
 np.set_printoptions(threshold=sys.maxsize)
 
-def pixelIsAPoint(pixel, pixelColors):
+def downScaleImage(image: Image) -> Image:
+    sizeArr = image.size
+    while(sizeArr[0]*sizeArr[1] > 50000):
+        sizeArr = list(map(lambda x : math.floor(x/5), sizeArr))
+    lowResImage = image.resize(sizeArr, Image.ANTIALIAS)
+    return lowResImage
+
+def pixelIsAPoint(pixel: list[int], pixelColors: list[list[int]]) -> bool:
     pixel = [pixel[0], pixel[1], pixel[2]]
     for i in range (0, len(pixelColors)):
         curColor = pixelColors[i]
         if(pixelColorMatch(curColor, pixel)): return True
     return False
 
-def pixelColorMatch(p1, p2):
+def pixelColorMatch(p1: list[int], p2: list[int]) -> bool:
     for i in range(0, len(p1)):
         if(abs(p1[i]-p2[i]) > 10): return False
     return True    
 
-def detectPoints(image, pointsClr):
+def detectPoints(image: Image, pointsClr: list[list[int]]) -> list[list[int]]:
     pointLocationArr = []
     pixelArr = np.asarray(image)
     for i in range(0, len(pixelArr)):
